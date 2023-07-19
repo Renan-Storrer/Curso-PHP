7d1fab2e10a3061a5d99f6c7c4816a8f17716426
Renan-Storrer <storrerrenan@gmail.com>
2023-07-19 19:14:00 -0300

    feat: tipo primitivo number

--- Modulo-1/ex003/index.php
+++ Modulo-1/ex003/index.php
@@ -6,6 +6,10 @@
   <title>Tipos Primitivos</title>
 </head>
 <body>
-  
+  <h1>Teste de tipos primitivos</h1>
+  <?php 
+    $number = 300;
+    echo "O valor da variável é $number";
+  ?>
 </body>
 </html>
