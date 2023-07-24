5387e06e16b09f24a7cd5296a1d0675b4f908bd2
Renan-Storrer <storrerrenan@gmail.com>
2023-07-24 19:59:35 -0300

    feat: cria formulario inicial

--- Modulo-1/ex004/index.php
+++ Modulo-1/ex004/index.php
@@ -6,6 +6,13 @@
   <title>Formulários</title>
 </head>
 <body>
-  
+  <header>
+    <h1>Apresente-se para nós</h1>
+  </header>
+  <section>
+    <form action="cad.php" method="get">
+      
+    </form>
+  </section>
 </body>
 </html>
