140fddbe4d8f33436c348e51b6510df4f13b51a2
Renan-Storrer <storrerrenan@gmail.com>
2023-07-25 19:53:56 -0300

    feat: cria e implementa o código para mostrar o numero sucessor e antecessor do numero escolhido

---
+++ Modulo-1/desafios/01-antecessor-e-sucessor/aes.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Resultado</title>
+</head>
+<body>
+  <header>
+    <h1>Resultado Final</h1>
+  </header>
+  <main>
+    <?php 
+      $numero = $_GET["numero"] ?? "número não escolhido!";
+      $sucessor = $numero + 1;
+      $antecessor = $numero - 1;
+      echo "<p>O número escolhido foi $numero</p>";
+      echo "<p>O seu antecessor é $antecessor</p>";
+      echo "<p>O seu sucessor é $sucessor</p>"
+    ?>
+    <p><a href="javascript:history.go(-1)">Voltar</a></p>
+</main>
+</body>
+</html>
