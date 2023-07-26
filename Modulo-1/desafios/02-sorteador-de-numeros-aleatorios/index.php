b19fd0bf9a3b80b07ed18fed7ab68f65e24e2d71
Renan-Storrer <storrerrenan@gmail.com>
2023-07-26 19:54:42 -0300

    feat: implementa html básico para o desafio já com seus textos exibidos em tela

---
+++ Modulo-1/desafios/02-sorteador-de-numeros-aleatorios/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Npumeros Aleatórios</title>
+</head>
+<body>
+  <main>
+    <h1>Trabalhando com números aleatórios</h1>
+    <p>Gerando um número aleatório entre 0 e 100..</p>
+    <p>
+      <?php 
+
+      ?>
+    </p>
+    <button>Gerar outro número</button>
+  </main>
+</body>
+</html>
